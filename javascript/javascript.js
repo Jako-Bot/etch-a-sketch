4e262ffd05b16ba56fe6e3e04bcafbe14cdcf3e9
Jakob Lemmer <jakob.lemmer@gmx.at>
2022-10-04 01:56:29 +0200

    Add rainbow-button and functionality

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,5 +1,19 @@
-function createGrid(num) {
+function mouseoverFcn() {
+    this.style.backgroundColor = getColor();
+}
 
+function getColor() {
+    if (drawColor === "lightgreen") return "lightgreen";
+    else if (drawColor === "rainbow") { 
+        return rainbowColors[Math.floor(Math.random() * 13)];
+    }
+}
+
+const rainbowColors = ["crimson", "deeppink", "gold", "peachpuff", "lavender",
+                        "orchid", "blueviolet", "darkslateblue", "lime",
+                        "darkgreen", "aqua", "beige", "slategrey"];
+
+function createGrid(num) {
     let grid = document.createElement("div");
     grid.classList.add("grid");
     grid.setAttribute("id", "grid");
@@ -14,15 +28,11 @@ function createGrid(num) {
             gridElement.style.height = parseInt(940/num) + 'px';
             gridElement.style.width = parseInt(940/num) + 'px';
 
-            gridElement.addEventListener("mouseover", () => {
-                gridElement.classList.add("gridElementActive");
-            });
+            gridElement.addEventListener("mouseover", mouseoverFcn);
             gridRow.appendChild(gridElement);
         }
-
         grid.appendChild(gridRow);
-    };
-    
+    };   
     document.body.appendChild(grid);
 }
 
@@ -31,9 +41,26 @@ const btn = document.getElementById("generateGrid");
 btn.addEventListener("click", () => {
     let grid = document.getElementById("grid");
     if (body.contains(grid)) {
+        let allElements = document.querySelectorAll(".gridElement");
+        for (element of allElements) {
+            if(element.classList.contains("gridElementActive")) {
+            element.removeEventListener("click", stdEventListenerFcn);
+            }
+        }
         body.removeChild(grid);
     }
-
     createGrid(prompt("Enter a value < 100 for your grid width: "));
-
 });
+
+
+let drawColor = "lightgreen";
+
+const btnStandard = document.getElementById("standard");
+btnStandard.addEventListener("click", () => {
+    drawColor = "lightgreen";
+});
+const btnRainbow = document.getElementById("rainbow");
+btnRainbow.addEventListener("click", () => {
+    drawColor = "rainbow";
+});
+
