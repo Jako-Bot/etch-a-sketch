8d2b5ffbc3c7a59b38c3c60f2578844105689761
Jakob Lemmer <jakob.lemmer@gmx.at>
2022-10-03 03:28:00 +0200

    Add create-grid-button and color-change on mouse-over

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,9 @@
 function createGrid(num) {
+
+    let grid = document.createElement("div");
+    grid.classList.add("grid");
+    grid.setAttribute("id", "grid");
+
     for (let row = 1; row <= num; row++) {
         const gridRow = document.createElement("div");
         gridRow.classList.add("gridRow");
@@ -6,18 +11,29 @@ function createGrid(num) {
         for (let column = 1; column <= num; column++) {
             const gridElement = document.createElement("div");
             gridElement.classList.add("gridElement");
-            gridElement.style.height = parseInt((940-(num+1))/num) + 'px';
-            gridElement.style.width = parseInt((940-(num+1))/num) + 'px';
+            gridElement.style.height = parseInt(940/num) + 'px';
+            gridElement.style.width = parseInt(940/num) + 'px';
 
+            gridElement.addEventListener("mouseover", () => {
+                gridElement.classList.add("gridElementActive");
+            });
             gridRow.appendChild(gridElement);
-
         }
 
         grid.appendChild(gridRow);
     };
-
+    
+    document.body.appendChild(grid);
 }
 
-const grid = document.querySelector("#grid");
+const body = document.querySelector("body");
+const btn = document.getElementById("generateGrid");
+btn.addEventListener("click", () => {
+    let grid = document.getElementById("grid");
+    if (body.contains(grid)) {
+        body.removeChild(grid);
+    }
 
-createGrid(64);
+    createGrid(prompt("Enter a value < 100 for your grid width: "));
+
+});
